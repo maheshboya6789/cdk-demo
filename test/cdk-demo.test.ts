6bf32eddd62b35b9f480fdde2728ca4e3192a8ff
Karthick.S <karthick.s@tranetechnologies.com>
2022-02-04 15:29:16 +0530

    Test case changes

--- test/cdk-demo.test.ts
+++ test/cdk-demo.test.ts
@@ -1,17 +1,29 @@
-// import { Template } from '@aws-cdk/assertions';
-// import * as cdk from '@aws-cdk/core';
-// import * as CdkDemo from '../lib/cdk-demo-stack';
+import { Template}from '@aws-cdk/assertions';
+import * as cdk from '@aws-cdk/core';
+import * as cdkdemo from '../lib/cdk-demo-stack'
+import { SynthUtils} from '@aws-cdk/assert';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-demo-stack.ts
-test('SQS Queue Created', () => {
-    //   const app = new cdk.App();
-    //     // WHEN
-    //   const stack = new CdkDemo.CdkDemoStack(app, 'MyTestStack');
-    //     // THEN
-    //   const template = Template.fromStack(stack);
-    
-    //   template.hasResourceProperties('AWS::SQS::Queue', {
-    //     VisibilityTimeout: 300
-    //   });
-    });
+// unit testing 
+const app = new cdk.App();
+// fine grained
+
+test('test to check the queue name',()=>{
+
+const stack = new cdkdemo.CdkDemoStack(app,'Myteststack');
+
+const template = Template.fromStack(stack);
+
+template.hasResourceProperties('AWS::SQS::Queue',{
+    QueueName: 'demo-jan29'
+});
+
+});
+
+
+// snapshot
+
+test('test app for snapshot',()=>{
+    const stack = new cdkdemo.CdkDemoStack(app,'snapshottest');
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+
+});
